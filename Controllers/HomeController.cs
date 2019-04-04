731534765afe2bc01daadf03579006cef8b0cb96
He Ai <heai0210@gmail.com>
2019-04-04 15:26:06 -0700

    razor

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+namespace RazorFun {
+    public class HomeController : Controller {
+
+        [HttpGet("")]
+        public IActionResult Index() => View();
+    }
+}
